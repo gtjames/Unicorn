747ae7725ee554ab60831190d879472d2a1aac4c
gtjames <gtjames@gmail.com>
2021-11-23 11:40:33 -0600

    added the location to the completeRequest

--- js/ride.js
+++ js/ride.js
@@ -29,9 +29,7 @@ WildRydes.map = WildRydes.map || {};
                 }
             }),
             contentType: 'application/json',
-            success:function ajaxSuccess(result) {
-                completeRequest(result, pickupLocation)
-            },
+            success: result => completeRequest(result, pickupLocation),
             error: function ajaxError(jqXHR, textStatus, errorThrown) {
                 console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
                 console.error('Response: ', jqXHR.responseText);
@@ -43,6 +41,7 @@ WildRydes.map = WildRydes.map || {};
     function completeRequest(result, pickupLocation) {
         var unicorn;
         var pronoun;
+        getWeather(pickupLocation)
         console.log('Response received from API: ', result);
         unicorn = result.Unicorn;
         pronoun = unicorn.Gender === 'Male' ? 'his' : 'her';
@@ -54,6 +53,53 @@ WildRydes.map = WildRydes.map || {};
             $('#request').text('Set Pickup');
         });
     }
+
+    function getWeather(loc) {
+        let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${loc.latitude}&lon=${loc.longitude}&exclude=minutely,hourly&appid=a099a51a6362902523bbf6495a0818aa`;
+        fetch(url)
+            .then(response => response.json())  //  wait for the response and convert it to JSON
+            .then(weather => {                  //  with the resulting JSON data do something
+
+                //  If the city was entered extract weather based on that API else use the LatLon API result format
+                let wx = latLonToWeather(weather);
+
+                //  We have converted the Lon Lat API (onecall) and City API (forecast) requests to the same format
+                for (let day of wx.daily) {
+                    //  let's build a nice card for each day of the weather data
+                    //  this is a GREAT opportunity to Reactify this code. But for now I will keep it simple
+                    innerHTML = `<h2>Date: ${day.date}</h2>
+                            <h4>Temp: Low ${day.min}&deg; / High: ${day.max}&deg;</h4>
+                            <p>Forecast: <img src='http://openweathermap.org/img/wn/${day.icon}@2x.png' alt=""> ${day.description}</p>
+                            <p>Chance of rain at ${day.pop}%</p>
+                            <p>Wind at ${day.wind_speed} mph out of the ${day.windDirection}</p>
+                            <p>Sunrise: ${day.sunrise} / Sunset: ${day.sunset}</p>`;
+                }
+            });
+    }
+
+    function latLonToWeather(data) {
+        let wx = {};
+        wx.daily = data.daily.map(d => ({
+            date:           niceDate(d.dt,data.timezone_offset),
+            min:            KtoF(d.temp.min),
+            max:            KtoF(d.temp.max),
+            sunrise:        niceTime(d.sunrise, data.timezone_offset),
+            sunset:         niceTime(d.sunset, data.timezone_offset),
+            icon:           d.weather[0].icon,
+            description:    d.weather[0].description,
+            wind_speed:     d.wind_speed.toFixed(0),
+            windDirection:  windDirection(d.wind_deg),
+            pop:            (d.pop * 100).toFixed(0),
+            feels_like:     KtoF(d.feels_like.day),
+            dewPoint:       d.dew_point,
+            humidity:       d.humidity,
+        }));
+        wx.city = "";
+        wx.lat  = data.lat;
+        wx.lon  = data.lon;
+        return wx;
+    }
+
 
     // Register click handler for #request button
     $(function onDocReady() {
