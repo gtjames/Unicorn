b18b1b38f2fcc1d4b6b403347fabf999591e9e89
gtjames <gtjames@gmail.com>
2022-11-11 12:16:44 -0600

    dry run

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_Fd4ORxxou',  // 'us-east-2_jZ530rqFB', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'ria3bkikjbt7g4havdg7bvhd8',   //'61omq5vvvlqjfp5caqmns1ghea', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-1_EojW9dDLF',  // 'us-east-2_Fd4ORxxou',  // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'k606g3eph2rv3njpicstkn441',  // 'ria3bkikjbt7g4havdg7bvhd8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://9fol3k4b12.execute-api.us-east-2.amazonaws.com/dev' // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
