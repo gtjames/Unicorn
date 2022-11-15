aa967ae719aef71657982f6a806355f90677f545
gtjames <gtjames@gmail.com>
2022-11-15 15:54:16 -0600

    setting the config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,21 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-west-2_1nkR8JwGq',  //'us-east-2_Fd4ORxxou',          //'us-east-1_EojW9dDLF',  //  e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '6bm3dlm05uo7l0cn3g2incrbld',   //'ria3bkikjbt7g4havdg7bvhd8',    // '6k606g3eph2rv3njpicstkn441',  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         'us-west-2_LEIWBjZjd',          //  e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '3ofn02b4rihsv6a91b1lmf5ge6',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-west-2'                     // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://9fol3k4b12.execute-api.us-east-2.amazonaws.com/dev' // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
+// window._config = {
+//     cognito: {
+//         userPoolId:         'us-west-2_1nkR8JwGq',  //'us-east-2_Fd4ORxxou',          //'us-east-1_EojW9dDLF',  //  e.g. us-east-2_uXboG5pAb
+//         userPoolClientId:   '6bm3dlm05uo7l0cn3g2incrbld',   //'ria3bkikjbt7g4havdg7bvhd8',    // '6k606g3eph2rv3njpicstkn441',  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+//         region:             'us-west-2'                     // e.g. us-east-2
+//     },
+//     api: {
+//         invokeUrl:          'https://av46hsrmk3.execute-api.us-west-2.amazonaws.com/prod'   //  https://9fol3k4b12.execute-api.us-east-2.amazonaws.com/dev' // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+//     }
+// };
