83209b2738de4a43ecff8f9272351cd8409ae5a3
gtjames <gtjames@gmail.com>
2022-11-15 08:22:32 -0600

    setting the config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_EojW9dDLF',  // 'us-east-2_Fd4ORxxou',  // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'k606g3eph2rv3njpicstkn441',  // 'ria3bkikjbt7g4havdg7bvhd8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId:         'us-east-2_Fd4ORxxou',          //'us-east-1_EojW9dDLF',  //  e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   'ria3bkikjbt7g4havdg7bvhd8',    // '6k606g3eph2rv3njpicstkn441',  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'                     // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://9fol3k4b12.execute-api.us-east-2.amazonaws.com/dev' // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://9fol3k4b12.execute-api.us-east-2.amazonaws.com/dev' // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
