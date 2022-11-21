b887138c510a47ff00faa6ae20d7c21b78737587
gtjames <gtjames@gmail.com>
2022-11-21 16:26:19 -0600

    Ready to go! For the teacher

--- js/config.js
+++ js/config.js
@@ -1,21 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-west-1_Ab2pzKfJx',  //  'us-west-2_LEIWBjZjd',          //  e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '495mgv53gebe92vr2jsn48g4pq',   //'3ofn02b4rihsv6a91b1lmf5ge6',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             'us-west-1'                     // e.g. us-east-2
+        userPoolId:         'us-west-2_Km7lwVZkq',          //  e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '65bbggcmskngcun0md5c5bdi8q',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-west-2'                     // e.g. us-east-2
     },
     api: {
-        invokeUrl:          'https://r22bpalx1k.execute-api.us-west-1.amazonaws.com/prod'  //  https://bswoadnczc.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl:          'https://q49a7a2a35.execute-api.us-west-2.amazonaws.com/prod'  //  https://bswoadnczc.execute-api.us-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
-
-// window._config = {
-//     cognito: {
-//         userPoolId:         'us-west-2_1nkR8JwGq',  //'us-east-2_Fd4ORxxou',          //'us-east-1_EojW9dDLF',  //  e.g. us-east-2_uXboG5pAb
-//         userPoolClientId:   '6bm3dlm05uo7l0cn3g2incrbld',   //'ria3bkikjbt7g4havdg7bvhd8',    // '6k606g3eph2rv3njpicstkn441',  // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-//         region:             'us-west-2'                     // e.g. us-east-2
-//     },
-//     api: {
-//         invokeUrl:          'https://av46hsrmk3.execute-api.us-west-2.amazonaws.com/prod'   //  https://9fol3k4b12.execute-api.us-east-2.amazonaws.com/dev' // 'https://3stehybb7a.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-//     }
-// };
